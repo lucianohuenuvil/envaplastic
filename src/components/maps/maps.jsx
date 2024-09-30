7903b238a68f84b1abd2466baa5be83651f2f5e6
unknown <luciano.huenuvil.cz@gmail.com>
2024-09-30 18:55:12 -0300

    add maps

--- src/components/maps/maps.jsx
+++ src/components/maps/maps.jsx
@@ -4,7 +4,7 @@ export default function Maps() {
         <h2 className="text-xl font-bold text-black mb-4">¿Dónde estámos ubicados?</h2>
         <div className="w-full h-full">
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3330.5668970826378!2d-70.56677258424588!3d-33.40846180274575!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9662cedd0fa0de87%3A0x8fddeef43d721a48!2sAv.%20Apoquindo%206415%2C%20Las%20Condes%2C%20Regi%C3%B3n%20Metropolitana!5e0!3m2!1ses!2scl!4v1655149171940!5m2!1ses!2scl"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2349.5072498827585!2d-70.63897807118741!3d-33.61295741901143!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9662d9cc5fce1857%3A0xc2c47e725f62506b!2sLos%20Duraznos%20129%2C%208840058%20La%20Pintana%2C%20Regi%C3%B3n%20Metropolitana!5e0!3m2!1ses!2scl!4v1727733082268!5m2!1ses!2scl"
             width="100%"
             height="100%"
             style={{ border: 0 }}
