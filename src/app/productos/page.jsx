a12350570823d03ecfac44a2d301c64dcdfbeb36
Luciano <lucianobvx@gmail.com>
2024-10-20 20:19:36 -0300

    cambios varios

--- src/app/productos/page.jsx
+++ src/app/productos/page.jsx
@@ -6,148 +6,176 @@ const products = [
   {
     id: '1',
     imagen: '/images/productos/botella1.png',
+    disponible: true,
     detalles: {
       nombre: {
         label: 'Nombre',
-        descripcion: 'ENVASE PET DESECHABLE 5 LITROS'
+        descripcion: 'ENVASE 3 LITROS UNIVERSAL'
+      },
+      tipoFabricacion:{
+        label: 'Tipo de Fabricación',
+        descripcion: 'Soplado'
       },
       material: {
-        label: 'Material: ',
+        label: 'Material',
         descripcion: 'Polietileno Tereftalato (PET)'
       },
       color: {
-        label: 'Color: ',
+        label: 'Color',
         descripcion: 'Incoloro'
       },
       tara: {
         label: 'Tara',
-        descripcion: 'ENVASE PET DESECHABLE 5 LITROS'
+        descripcion: '94 +/- 2 gramos'
       },
-      taraTA: {
-        label: 'Tara con Tapa y Asa: ',
-        descripcion: '105 gramos'
-      },
-      diametro: {
-        label: 'Diametro: ',
-        descripcion: 'Incoloro'
-      },
-      altura: {
-        label: 'Altura: ',
-        descripcion: 'ENVASE PET DESECHABLE 5 LITROS'
-      },
+      // taraTA: {
+      //   label: 'Tara con Tapa y Asa: ',
+      //   descripcion: '105 gramos'
+      // },
+      // diametroCuello: {
+      //   label: 'Diámetro del cuello',
+      //   descripcion: '45 +/- 0.25mm'
+      // },
+      // diametro: {
+      //   label: 'Diámetro',
+      //   descripcion: '155 mm'
+      // },
+      // altura: {
+      //   label: 'Altura: ',
+      //   descripcion: '340 +/- 2mm'
+      // },
       capacidad: {
-        label: 'Material: ',
-        descripcion: '6 Litros'
+        label: 'Capacidad',
+        descripcion: '3 Litros'
       },
       aroma: {
-        label: 'Aroma: ',
+        label: 'Aroma',
         descripcion: 'Sin olor'
       },
       descripcion: {
-        label: 'Descripción : ',
-        descripcion: 'Botellón de Polierileno Tereftalato (PET) con capacidad para 6 Litros, testeado y certificado de origen, es apto para envasado de líquidos y libre de Bisfenol A (BPA)'
+        label: 'Descripción',
+        descripcion: 'Descubre nuestro práctico y versátil envase PET de 3 litros, diseñado para ofrecerte la mejor solución de almacenamiento. Fabricado con material PET de alta calidad, este envase es ligero, resistente y completamente reciclable'
       },
     }
   },
   {
     id: '2',
     imagen: '/images/productos/botella2.png',
+    disponible: true,
     detalles: {
       nombre: {
         label: 'Nombre',
-        descripcion: 'ENVASE PET DESECHABLE 6 LITROS'
+        descripcion: 'ENVASE 5 LITROS DESECHABLE'
+      },
+      tipoFabricacion:{
+        label: 'Tipo de Fabricación',
+        descripcion: 'Soplado'
       },
       material: {
-        label: 'Material: ',
-        descripcion: ''
+        label: 'Material',
+        descripcion: 'Polietileno Tereftalato (PET)'
       },
       color: {
-        label: 'Color: ',
+        label: 'Color',
         descripcion: 'Incoloro'
       },
       tara: {
         label: 'Tara',
-        descripcion: 'ENVASE PET DESECHABLE 5 LITROS'
+        descripcion: '94 +/- 2 gramos'
       },
       taraTA: {
-        label: 'Material: ',
-        descripcion: ''
+        label: 'Tara con Tapa y Asa: ',
+        descripcion: '105 gramos'
       },
-      diametro: {
-        label: 'Diametro: ',
-        descripcion: 'Incoloro'
-      },
-      altura: {
-        label: 'Altura: ',
-        descripcion: 'ENVASE PET DESECHABLE 5 LITROS'
-      },
+      // diametroCuello: {
+      //   label: 'Diámetro del cuello',
+      //   descripcion: '45 +/- 0.25mm'
+      // },
+      // diametro: {
+      //   label: 'Diámetro',
+      //   descripcion: '155 mm'
+      // },
+      // altura: {
+      //   label: 'Altura: ',
+      //   descripcion: '340 +/- 2mm'
+      // },
       capacidad: {
-        label: 'Material: ',
-        descripcion: '6 Litros'
+        label: 'Capacidad',
+        descripcion: '5 Litros'
       },
       aroma: {
-        label: 'Aroma: ',
+        label: 'Aroma',
         descripcion: 'Sin olor'
       },
       descripcion: {
-        label: 'Descripción : ',
-        descripcion: 'Botellón de Polierileno Tereftalato (PET) con capacidad para 6 Litros, testeado y certificado de origen, es apto para envasado de líquidos y libre de Bisfenol A (BPA)'
+        label: 'Descripción',
+        descripcion: 'Botellón de Polierileno Tereftalato (PET) con capacidad para 5 Litros, testeado y certificado de origen, es apto para envasado de líquidos y libre de Bisfenol A (BPA)'
       },
     }
   },
   {
     id: '3',
     imagen: '/images/productos/botella3.png',
+    disponible: true,
     detalles: {
       nombre: {
         label: 'Nombre',
-        descripcion: 'ENVASE PET DESECHABLE 5 LITROS'
+        descripcion: 'ENVASE 3 LITROS GENÉRICO'
+      },
+      tipoFabricacion:{
+        label: 'Tipo de Fabricación',
+        descripcion: 'Soplado'
       },
       material: {
-        label: 'Material: ',
+        label: 'Material',
         descripcion: 'Polietileno Tereftalato (PET)'
       },
       color: {
-        label: 'Color: ',
+        label: 'Color',
         descripcion: 'Incoloro'
       },
       tara: {
         label: 'Tara',
-        descripcion: 'ENVASE PET DESECHABLE 5 LITROS'
+        descripcion: '94 +/- 2 gramos'
       },
-      taraTA: {
-        label: 'Tara con Tapa y Asa: ',
-        descripcion: '105 gramos'
-      },
-      diametro: {
-        label: 'Diametro: ',
-        descripcion: 'Incoloro'
-      },
-      altura: {
-        label: 'Altura: ',
-        descripcion: 'ENVASE PET DESECHABLE 5 LITROS'
-      },
+      // taraTA: {
+      //   label: 'Tara con Tapa y Asa: ',
+      //   descripcion: '105 gramos'
+      // },
+      // diametroCuello: {
+      //   label: 'Diámetro del cuello',
+      //   descripcion: '45 +/- 0.25mm'
+      // },
+      // diametro: {
+      //   label: 'Diámetro',
+      //   descripcion: '155 mm'
+      // },
+      // altura: {
+      //   label: 'Altura: ',
+      //   descripcion: '340 +/- 2mm'
+      // },
       capacidad: {
-        label: 'Material: ',
-        descripcion: '6 Litros'
+        label: 'Capacidad',
+        descripcion: '3 Litros'
       },
       aroma: {
-        label: 'Aroma: ',
+        label: 'Aroma',
         descripcion: 'Sin olor'
       },
       descripcion: {
-        label: 'Descripción : ',
-        descripcion: 'Botellón de Polierileno Tereftalato (PET) con capacidad para 6 Litros, testeado y certificado de origen, es apto para envasado de líquidos y libre de Bisfenol A (BPA)'
+        label: 'Descripción',
+        descripcion: 'Descubre nuestro práctico y versátil envase PET de 3 litros, diseñado para ofrecerte la mejor solución de almacenamiento. Fabricado con material PET de alta calidad, este envase es ligero, resistente y completamente reciclable.'
       },
     }
   },
   {
     id: '4',
     imagen: '/images/productos/botella4.png',
+    disponible: true,
     detalles: {
       nombre: {
         label: 'Nombre',
-        descripcion: 'ENVASE PET DESECHABLE 5 LITROS'
+        descripcion: 'ENVASE 6 LITROS DESECHABLE '
       },
       material: {
         label: 'Material: ',
@@ -159,30 +187,30 @@ const products = [
       },
       tara: {
         label: 'Tara',
-        descripcion: 'ENVASE PET DESECHABLE 5 LITROS'
+        descripcion: '94 +/- 2 gramos'
       },
       taraTA: {
         label: 'Tara con Tapa y Asa: ',
         descripcion: '105 gramos'
       },
-      diametro: {
-        label: 'Diametro: ',
-        descripcion: 'Incoloro'
+      diametroCuello: {
+        label: 'Diametro cuello',
+        descripcion: '45 +/- 0.25 mm'
       },
       altura: {
-        label: 'Altura: ',
-        descripcion: 'ENVASE PET DESECHABLE 5 LITROS'
+        label: 'Altura',
+        descripcion: '350 +/- 2 mm'
       },
       capacidad: {
-        label: 'Material: ',
+        label: 'Capacidad',
         descripcion: '6 Litros'
       },
       aroma: {
-        label: 'Aroma: ',
+        label: 'Aroma',
         descripcion: 'Sin olor'
       },
       descripcion: {
-        label: 'Descripción : ',
+        label: 'Descripción',
         descripcion: 'Botellón de Polierileno Tereftalato (PET) con capacidad para 6 Litros, testeado y certificado de origen, es apto para envasado de líquidos y libre de Bisfenol A (BPA)'
       },
     }
@@ -190,92 +218,102 @@ const products = [
   {
     id: '5',
     imagen: '/images/productos/botella5.png',
+    disponible: false,
     detalles: {
       nombre: {
         label: 'Nombre',
-        descripcion: 'ENVASE PET DESECHABLE 5 LITROS'
+        descripcion: 'ENVASE 5 LITROS DESECHABLE '
       },
       material: {
-        label: 'Material: ',
+        label: 'Material',
         descripcion: 'Polietileno Tereftalato (PET)'
       },
       color: {
-        label: 'Color: ',
+        label: 'Color',
         descripcion: 'Incoloro'
       },
       tara: {
         label: 'Tara',
-        descripcion: 'ENVASE PET DESECHABLE 5 LITROS'
+        descripcion: '94 +/- 2 gramos'
       },
       taraTA: {
-        label: 'Tara con Tapa y Asa: ',
+        label: 'Tara con Tapa y Asa',
         descripcion: '105 gramos'
       },
+      diametroCuello: {
+        label: 'Diámetro del cuello',
+        descripcion: '45 +/- 0.25mm'
+      },
       diametro: {
-        label: 'Diametro: ',
-        descripcion: 'Incoloro'
+        label: 'Diámetro',
+        descripcion: '155 mm'
       },
       altura: {
-        label: 'Altura: ',
-        descripcion: 'ENVASE PET DESECHABLE 5 LITROS'
+        label: 'Altura',
+        descripcion: '340 +/- 2mm'
       },
       capacidad: {
-        label: 'Material: ',
-        descripcion: '6 Litros'
+        label: 'Capacidad',
+        descripcion: '5 Litros'
       },
       aroma: {
-        label: 'Aroma: ',
+        label: 'Aroma',
         descripcion: 'Sin olor'
       },
       descripcion: {
-        label: 'Descripción : ',
-        descripcion: 'Botellón de Polierileno Tereftalato (PET) con capacidad para 6 Litros, testeado y certificado de origen, es apto para envasado de líquidos y libre de Bisfenol A (BPA)'
+        label: 'Descripción',
+        descripcion: 'Botellón de Polierileno Tereftalato (PET) con capacidad para 5 Litros, testeado y certificado de origen, es apto para envasado de líquidos y libre de Bisfenol A (BPA)'
       },
     }
   },
   {
     id: '6',
     imagen: '/images/productos/botella6.png',
+    disponible: true,
     detalles: {
       nombre: {
         label: 'Nombre',
-        descripcion: 'ENVASE PET DESECHABLE 5 LITROS'
+        descripcion: 'ENVASE 5 LITROS DESECHABLE'
       },
       material: {
-        label: 'Material: ',
+        label: 'Material',
         descripcion: 'Polietileno Tereftalato (PET)'
       },
       color: {
-        label: 'Color: ',
+        label: 'Color',
         descripcion: 'Incoloro'
       },
       tara: {
         label: 'Tara',
-        descripcion: 'ENVASE PET DESECHABLE 5 LITROS'
+        descripcion: '94 +/- 2 gramos'
       },
       taraTA: {
-        label: 'Tara con Tapa y Asa: ',
+        label: 'Tara con Tapa y Asa',
         descripcion: '105 gramos'
       },
+      diametroCuello: {
+        label: 'Diámetro del cuello',
+        descripcion: '45 +/- 0.25mm'
+      },
       diametro: {
-        label: 'Diametro: ',
-        descripcion: 'Incoloro'
+        label: 'Diámetro',
+        descripcion: '155 mm'
       },
       altura: {
-        label: 'Altura: ',
-        descripcion: 'ENVASE PET DESECHABLE 5 LITROS'
+        label: 'Altura',
+        descripcion: '340 +/- 2mm'
       },
       capacidad: {
-        label: 'Material: ',
-        descripcion: '6 Litros'
+        label: 'Capacidad',
+        descripcion: '5 Litros'
       },
       aroma: {
-        label: 'Aroma: ',
+        label: 'Aroma',
         descripcion: 'Sin olor'
       },
       descripcion: {
-        label: 'Descripción : ',
-        descripcion: 'Botellón de Polierileno Tereftalato (PET) con capacidad para 6 Litros, testeado y certificado de origen, es apto para envasado de líquidos y libre de Bisfenol A (BPA)'
+        label: 'Descripción',
+        descripcion: 'Botellón de Polierileno Tereftalato (PET) con capacidad para 5 Litros, testeado y certificado de origen, es apto para envasado de líquidos y libre de Bisfenol A (BPA)'
       },
     }
   },
@@ -331,11 +369,11 @@ const ProductosPage = () => {
       <div className="pt-4 bg-white">
         <div className="container max-w-7xl mx-auto px-4">
           <div className="text-center pb-4">
-            <h2 className="text-3xl font-bold text-gray-600 text-left">Productos</h2>
+            <h2 className="text-3xl font-bold text-gray-600 text-left">Envases PET:</h2>
           </div>
 
           {/* Listado de productos */}
-          <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4 mb-8">
+          <div className="grid grid-cols-2 gap-6 sm:grid-cols-3 md:grid-cols-4 mb-8">
             {products.map((product) => (
               <ProductCard key={product.id} product={product} />
             ))}
--- src/components/productCard/productCard.jsx
+++ src/components/productCard/productCard.jsx
@@ -11,25 +11,25 @@ const ProductCard = ({ product }) => {
 
   return (
     <section className="flex flex-col">
-      <div className="bg-white shadow-md  w-full max-w-[480px] sm:max-w-[400px] xs:max-w-[300px] transition-transform transform hover:scale-105 hover:shadow-xl">
+      <div className="bg-white shadow-md w-full max-w-[480px] sm:max-w-[400px] xs:max-w-[300px] transition-transform transform hover:scale-105 hover:shadow-xl rounded-lg">
         <img
           src={product.imagen}
           alt={product.name}
-          className="w-full h-[200px] lg:h-[300px] xs:h-[250px] object-contain transition-opacity duration-300 hover:opacity-90"
+          className="w-full bg-[#fdfdfd] h-[200px] lg:h-[300px] xs:h-[250px] object-contain transition-opacity duration-300 hover:opacity-90 rounded-t-lg"
         />
-        <div className="p-4">
-          <h3 className="sm:text-lg xs:text-xs font-semibold text-gray-800 text-center text-gray-400 mb-3">{product.detalles.nombre.descripcion}</h3>
+        <div className="">
+          {/* <h3 className="sm:text-lg xs:text-xs font-semibold text-gray-800 text-center text-gray-400 mb-3">{product.detalles.nombre.descripcion}</h3> */}
           <button
             onClick={openModal}
-            className="w-full bg-[#0369a1] hover:bg-[#164e63] text-gray-100 py-2 px-4 rounded-lg hover:from-blue-500 hover:to-cyan-500 transition-all duration-300 ease-in-out text-md xs:text-sm"
+            className="w-full bg-[#2b86b8] hover:bg-[#164e63] text-gray-100 py-3 px-6 hover:from-blue-500 hover:to-cyan-500 transition-all duration-300 ease-in-out text-md xs:text-sm rounded-b-lg"
           >
             Ver detalles
           </button>
         </div>
-
       </div>
-        {isModalOpen && <ProductModal product={product} onClose={closeModal} />}
+      {isModalOpen && <ProductModal product={product} onClose={closeModal} />}
     </section>
+
   );
 };
 
