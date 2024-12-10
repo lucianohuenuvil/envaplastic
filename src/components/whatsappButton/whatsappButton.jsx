a6714304593b04849061d1f73d0d8c60b4923c08
lucianohuenuvil <lucianobvx@gmail.com>
2024-12-09 22:21:44 -0300

    fix wsp message

--- src/components/whatsappButton/whatsappButton.jsx
+++ src/components/whatsappButton/whatsappButton.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export function WhatsAppButton() {
     return (
         <div className="fixed bottom-[30px] right-[40px] w-[60px] sm:w-[75px] z-50">
-            <Link target="_blank" href="https://api.whatsapp.com/send?phone=56988113986"
+            <Link target="_blank" href="https://api.whatsapp.com/send?phone=56988113986&text=Hola,%20quiero%20más%20información%20sobre%20sus%20productos."
                 className="flex bg-green-500 hover:bg-green-600 text-white rounded-full shadow-lg transition-all">
                 <img src="/logowsp.png" alt="WhatsApp Icon" loading="lazy" className="object-cover w-full h-full p-[12px]" />
             </Link>
